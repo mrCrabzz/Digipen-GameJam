638fa43b7d22688a343d42b9d06b44e22ddf52a3
KirillUstinov1 <67757408+KirillUstinov1@users.noreply.github.com>
2021-10-28 13:22:41 -0700

    guy dies
    lol he dead

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
 
     public static int Score = 0;
+    private bool amIdead;
     //public static int Health = 5;
-
     private static GameManager _instance;
+    private static GameObject player;
 
     public static GameManager instance
     {
@@ -39,12 +40,23 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("guy1");
+        amIdead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player == null && amIdead == false)
+        {
+            StartCoroutine(DelayStart());
+            amIdead = true;
+        }
+    }
+
+    IEnumerator DelayStart()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("endscreen");
     }
 }
